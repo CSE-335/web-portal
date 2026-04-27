ff80e2a410fe88653274ebde68659b44bf80f8f5
Jein <djeinabou.bodie@gmail.com>
2026-04-27 13:49:12 -0700

    Added Stellar Odyssey to Portal

--- games.config.mjs
+++ games.config.mjs
@@ -3,6 +3,7 @@ export const gameRepos = [
   'https://github.com/jeinjein60/Biology-Virus-AI-Game.git',
   'https://github.com/NatalieParker/human-motion-simulator.git',
   'https://github.com/etchre/python-programming-game.git',
+  'https://github.com/jeinjein60/Stellar-Odyssey.git',
   'https://github.com/atalania/Matrix-Meadow-Academy.git',
   'https://github.com/atalania/SonicLab.git',
 
